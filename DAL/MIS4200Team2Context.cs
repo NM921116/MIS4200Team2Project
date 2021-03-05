340d92cd52af152955e402b4a0c47e76475425ae
Nick Manuel <nm921116@ohio.edu>
2021-03-05 10:53:55 -0500

    This is a pain in the butt

---
+++ DAL/MIS4200Team2Context.cs
@@ -0,0 +1,18 @@
+﻿using MIS4200Team2Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team2Project.DAL
+{
+    public class MIS4200Team2Context    :   DbContext
+    {
+        public MIS4200Team2Context()    :   base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Profile> profile { get; set; }
+    }
+}
---
+++ Models/Profile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team2Project.Models
+{
+    public class Profile
+    {
+        public Guid profileID { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string fullName
+        {
+            get
+            {
+                return lastName + ", " + firstName;
+            }
+        }
+        public DateTime hireDate { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string position { get; set; }
+        public string operatingGroup { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string bio { get; set; }
+    }
+}
